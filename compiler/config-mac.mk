32cf98d607102274d2efc03787b56b9b70b3e0fc
Louis Carbo Estaque <louis@carbo-estaque.fr>
2026-03-05 10:12:20 +0100

    Remove config.mk from git tracking
    Add warning to Makefile + Add example MacOS config
    
    Remove config.mk from git tracking

---
+++ compiler/config-mac.mk
@@ -0,0 +1,3 @@
+ANTLRJAR=/opt/homebrew/opt/antlr/antlr-4.13.2-complete.jar
+ANTLRINC=/opt/homebrew/opt/antlr4-cpp-runtime/include/antlr4-runtime
+ANTLRLIB=-L/opt/homebrew/opt/antlr4-cpp-runtime/lib -lantlr4-runtime
