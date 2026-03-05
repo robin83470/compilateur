be94cb7aa314e0771e9c45d08809941f37f52f16
DarkoSancho <tomek@Tom.localdomain>
2026-03-05 11:09:52 +0100

    Add: test 4.6

---
+++ ifcc-test-output/testfiles-8_declaration/asm-ifcc.s
@@ -0,0 +1,11 @@
+# coucou1
+.globl main
+ main: 
+# prologue
+pushq %rbp # save %rbp on the stack
+movq %rsp, %rbp # define %rbp for the current function
+    movl $4, -0(%rbp)
+    movl $0, %eax
+# epilogue
+popq %rbp # restore %rbp from the stack
+    ret
