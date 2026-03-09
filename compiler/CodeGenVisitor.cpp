2c33473964aea36dc4acb38efd7978554dff0935
robin83470 <robin.renous83@gmail.com>
2026-03-09 11:54:02 +0100

    jsp

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -116,3 +116,15 @@ antlrcpp::Any CodeGenVisitor::visitExpr_plusmoins(ifccParser::Expr_plusmoinsCont
         }
         return 0;
 }
+
+antlrcpp::Any CodeGenVisitor::visitExpr_moinsunaire(ifccParser::Expr_moinsunaireContext *ctx)
+{
+    visit(ctx->rhs());              // résultat dans %eax
+    std::cout << "    negl %eax\n"; // %eax = -%eax
+    return 0;
+}
+
+antlrcpp::Any CodeGenVisitor::visitExpr_parenthese(ifccParser::Expr_parentheseContext *ctx)
+{
+    return visit(ctx->rhs());
+}
