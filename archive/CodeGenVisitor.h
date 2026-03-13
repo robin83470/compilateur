e1119982c0089439b48041ad89a38ef1b95c6593
Louis Carbo Estaque <louis@carbo-estaque.fr>
2026-03-13 08:38:20 +0100

    Add back CodeGenVisitor (to see archive code)

---
+++ archive/CodeGenVisitor.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "antlr4-runtime.h"
+#include "generated/ifccBaseVisitor.h"
+#include "SymbolTableVisitor.h"
+
+class CodeGenVisitor : public ifccBaseVisitor
+{
+private:
+        std::map<std::string, SymbolTableVisitor::SymbolInfo> symbolTable;
+        int hasReturn =0;
+
+public:
+        CodeGenVisitor(std::map<std::string, SymbolTableVisitor::SymbolInfo> symbols);
+
+        virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override;
+        virtual antlrcpp::Any visitDeclarator(ifccParser::DeclaratorContext *ctx) override;
+        virtual antlrcpp::Any visitDeclaration_stmt(ifccParser::Declaration_stmtContext *ctx) override;
+        virtual antlrcpp::Any visitAssign_stmt(ifccParser::Assign_stmtContext *ctx) override;
+        virtual antlrcpp::Any visitReturn_stmt(ifccParser::Return_stmtContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_plusmoins(ifccParser::Expr_plusmoinsContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_const(ifccParser::Expr_constContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_char(ifccParser::Expr_charContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_id(ifccParser::Expr_idContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_multdiv(ifccParser::Expr_multdivContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_moinsunaire(ifccParser::Expr_moinsunaireContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_parenthese(ifccParser::Expr_parentheseContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_and(ifccParser::Expr_andContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_or(ifccParser::Expr_orContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_xor(ifccParser::Expr_xorContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_comparison(ifccParser::Expr_comparisonContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_equality(ifccParser::Expr_equalityContext *ctx) override;
+
+        // virtual antlrcpp::Any visitRhs(ifccParser::RhsContext *ctx) override
+        // {
+        //         return visitChildren(ctx);
+        // }
+
+};
