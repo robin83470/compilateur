c2a65444c9b7aff166198a36572048ecf680d024
Xx-DarkBGdu69-xX <marcsauce02@gmail.com>
2026-03-19 09:46:27 +0100

    Modification des visiteurs pour pointeurs

--- compiler/SymbolTableVisitor.h
+++ compiler/SymbolTableVisitor.h
@@ -11,13 +11,19 @@ class SymbolTableVisitor : public ifccBaseVisitor {
         virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override;
         virtual antlrcpp::Any visitDeclaration_stmt(ifccParser::Declaration_stmtContext *ctx) override;
         virtual antlrcpp::Any visitDeclarator(ifccParser::DeclaratorContext *ctx) override;
+        virtual antlrcpp::Any visitPointer_prefix(ifccParser::Pointer_prefixContext *ctx) override;
         virtual antlrcpp::Any visitAssign_stmt(ifccParser::Assign_stmtContext *ctx) override;
         virtual antlrcpp::Any visitReturn_stmt(ifccParser::Return_stmtContext *ctx) override;
+        virtual antlrcpp::Any visitLvalue_id(ifccParser::Lvalue_idContext *ctx) override;
+        virtual antlrcpp::Any visitLvalue_deref(ifccParser::Lvalue_derefContext *ctx) override;
+        virtual antlrcpp::Any visitLvalue_parenthese(ifccParser::Lvalue_parentheseContext *ctx) override;
         virtual antlrcpp::Any visitExpr_moinsunaire(ifccParser::Expr_moinsunaireContext *ctx) override;
         virtual antlrcpp::Any visitExpr_id(ifccParser::Expr_idContext *ctx) override;
         virtual antlrcpp::Any visitExpr_plusmoins(ifccParser::Expr_plusmoinsContext *ctx) override;
         virtual antlrcpp::Any visitExpr_multdiv(ifccParser::Expr_multdivContext *ctx) override;
         virtual antlrcpp::Any visitExpr_parenthese(ifccParser::Expr_parentheseContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_addrof(ifccParser::Expr_addrofContext *ctx) override;
+        virtual antlrcpp::Any visitExpr_deref(ifccParser::Expr_derefContext *ctx) override;
         virtual antlrcpp::Any visitExpr_const(ifccParser::Expr_constContext *ctx) override;
         virtual antlrcpp::Any visitExpr_char(ifccParser::Expr_charContext *ctx) override;
         virtual antlrcpp::Any visitExpr_comparison(ifccParser::Expr_comparisonContext *ctx) override;
@@ -29,4 +35,7 @@ class SymbolTableVisitor : public ifccBaseVisitor {
         virtual antlrcpp::Any visitExpr_putchar(ifccParser::Expr_putcharContext *ctx) override;
     private:
         void checkVariableUsed(const std::string& varName);
+        void requireType(const std::string& actual, const std::string& expected, const std::string& where);
+        std::string addPointerLevel(const std::string& type) const;
+        std::string removePointerLevel(const std::string& type, const std::string& where) const;
 };
