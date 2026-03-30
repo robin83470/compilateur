99d7042c71eabf1a2e04fdf5bbe3c222e9d87a88
Daniel Massila <daniel.massila-akendengue@insa-lyon.fr>
2026-03-31 00:24:00 +0200

    add visitor for logical AND

--- compiler/IRVisitor.cpp
+++ compiler/IRVisitor.cpp
@@ -679,3 +679,53 @@ antlrcpp::Any IRVisitor::visitRhsList(ifccParser::RhsListContext* ctx) {
     }
     return args;
 }
+
+antlrcpp::Any IRVisitor::visitExpr_land(ifccParser::Expr_landContext *ctx) {
+    // Variable contenant le résultat de (a && b)
+    std::string res = currentCFG->newTemp("int");
+
+    // Blocs pour évaluer la partie gauche (a) et la partie droite (b)
+    IRBasicBloc* evalLeftBloc  = currentCFG->getCurrentBasicBloc();
+    IRBasicBloc* evalRightBloc = currentCFG->addBasicBlocUnique("land_rhs");
+
+    // Blocs pour fixer le résultat à 0 si False et 1 si True
+    IRBasicBloc* setFalseBloc  = currentCFG->addBasicBlocUnique("land_false");
+    IRBasicBloc* setTrueBloc   = currentCFG->addBasicBlocUnique("land_true");
+
+    // Bloc où on continue après l'expression
+    IRBasicBloc* afterExprBloc = currentCFG->addBasicBlocUnique("land_after");
+
+    // Visiter l'expression de gauche a
+    std::string leftVar = std::any_cast<std::string>(visit(ctx->rhs(0)));
+
+    // Si l'évaluation de a donne faux, on saute directement vers le bloc false sans évaluer b, sinon on évalue l'expression de droite b
+    evalLeftBloc->setTestVarName(leftVar);
+    evalLeftBloc->setExitTrue(evalRightBloc);
+    evalLeftBloc->setExitFalse(setFalseBloc);
+
+    // On bascule dans le bloc pour évaluer l'expression b
+    currentCFG->setCurrentBasicBloc(evalRightBloc);
+
+    std::string rightVar = std::any_cast<std::string>(visit(ctx->rhs(1)));
+
+    // Si l'évaluation de b donne faux, on saute vers le bloc false sinon vers true
+    evalRightBloc->setTestVarName(rightVar);
+    evalRightBloc->setExitTrue(setTrueBloc);
+    evalRightBloc->setExitFalse(setFalseBloc);
+
+
+    // Bloc résultat faux (res = 0)
+    currentCFG->setCurrentBasicBloc(setFalseBloc);
+    setFalseBloc->addInstruction(new IRInstrConst(setFalseBloc, res, 0));
+    setFalseBloc->setExitTrue(afterExprBloc);
+
+    // Bloc résultat vrai (res = 1)
+    currentCFG->setCurrentBasicBloc(setTrueBloc);
+    setTrueBloc->addInstruction(new IRInstrConst(setTrueBloc, res, 1));
+    setTrueBloc->setExitTrue(afterExprBloc);
+
+    // Reprise vers la suite du programme après l'expression
+    currentCFG->setCurrentBasicBloc(afterExprBloc);
+
+    return res;
+}
--- compiler/IRVisitor.h
+++ compiler/IRVisitor.h
@@ -57,6 +57,7 @@ public:
     virtual antlrcpp::Any visitExpr_putchar(ifccParser::Expr_putcharContext* ctx) override;
     virtual antlrcpp::Any visitExpr_funcCall(ifccParser::Expr_funcCallContext* ctx) override;
     virtual antlrcpp::Any visitRhsList(ifccParser::RhsListContext* ctx) override;
+    virtual antlrcpp::Any visitExpr_land(ifccParser::Expr_landContext *ctx) override;
 private:
     std::vector<std::pair<IRControlFlowGraph*, SymbolTable*>> allFunctions;
     struct LoopContext {
